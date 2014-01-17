9979e1bff31b54803913d4a20d7c9e0a6e2d16ca
Kahmali Rose <krose72205@gmail.com>
2014-01-17 18:14:53 -0500

    Added stub for AuthenticationActivity

---
+++ src/com/overthink/mechmaid/auth/AuthenticationActivity.java
@@ -0,0 +1,15 @@
+package com.overthink.mechmaid.auth;
+
+import android.app.Activity;
+import android.os.Bundle;import com.overthink.mechmaid.R;
+
+/**
+ * User: KodeRiter
+ * Date: 1/17/14
+ */
+public class AuthenticationActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
