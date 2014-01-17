1867a0253ed336b948390e032213cfcce15e183b
Anthony Reid <AnthonyReid99@gmail.com>
2014-01-17 16:57:53 -0500

    Created account authenticator class

---
+++ src/com/overthink/mechmaid/auth/AccountAuthenticator.java
@@ -0,0 +1,9 @@
+package com.overthink.mechmaid.auth;
+
+/**
+ * Fulfils authenication-related tasks such as getting the auth token
+ * and presenting a login screen to the user
+ */
+public class AccountAuthenticator {
+
+}
